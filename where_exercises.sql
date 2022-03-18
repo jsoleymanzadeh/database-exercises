d2e8ab4df7eaf414d9f2c8dbbd0eea1fde3feae9
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-03-18 09:15:32 -0500

    added where exercises

---
+++ where_exercises.sql
@@ -0,0 +1,5 @@
+USE employees;
+SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
