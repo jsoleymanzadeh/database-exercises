335cda9d9f056c204997b97de32db008f5c6856c
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-03-18 15:55:46 -0500

    reformatted for readability

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,12 @@
 USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
-CREATE TABLE albums (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50),
-    name VARCHAR(100),
+CREATE TABLE albums
+(
+    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist       VARCHAR(50),
+    name         VARCHAR(100),
     release_date INT UNSIGNED,
-    sales DECIMAL(4, 1),
-    genre VARCHAR(50),
+    sales        DECIMAL(4, 1),
+    genre        VARCHAR(50),
     PRIMARY KEY (id)
 );
--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,6 +1,22 @@
 USE employees;
-SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M' ORDER BY last_name DESC, first_name DESC;
-SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
-SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
-SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
-SELECT * FROM employees WHERE last_name LIKE '%e%' ORDER BY emp_no DESC;
+SELECT *
+FROM employees
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+  AND gender = 'M'
+ORDER BY last_name DESC, first_name DESC;
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+   OR last_name LIKE '%E';
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E';
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
+SELECT *
+FROM employees
+WHERE last_name LIKE '%e%'
+ORDER BY emp_no DESC;
