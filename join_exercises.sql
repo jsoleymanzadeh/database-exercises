9fe540e78dec8c71403ccbc02d2a1b74376e75b8
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-03-22 13:46:13 -0500

    added join exercises

---
+++ join_exercises.sql
@@ -0,0 +1,40 @@
+USE employees;
+SELECT dept_name AS 'DEPARTMENT NAME', CONCAT(first_name, ' ', last_name) AS 'DEPARTMENT MANAGER'
+FROM employees AS e
+         JOIN dept_manager AS dm ON e.emp_no = dm.emp_no
+         JOIN departments AS d ON dm.dept_no = d.dept_no
+WHERE dm.to_date = '9999-01-01';
+SELECT dept_name AS 'DEPARTMENT NAME', CONCAT(first_name, ' ', last_name) AS 'DEPARTMENT MANAGER'
+FROM employees AS e
+         JOIN dept_manager AS dm ON e.emp_no = dm.emp_no
+         JOIN departments AS d ON dm.dept_no = d.dept_no
+WHERE dm.to_date = '9999-01-01'
+  AND gender = 'F';
+SELECT title, COUNT(title) AS 'Total'
+FROM titles AS t
+         JOIN dept_emp AS de ON t.emp_no = de.emp_no
+         JOIN departments AS d ON de.dept_no = d.dept_no
+WHERE t.to_date = '9999-01-01'
+  AND d.dept_name = 'Customer Service'
+GROUP BY title;
+SELECT dept_name                          AS 'Department Name',
+       CONCAT(first_name, ' ', last_name) AS 'Department Manager',
+       salary                             AS 'Salary'
+FROM employees e
+         JOIN salaries s on e.emp_no = s.emp_no
+         JOIN dept_manager dm on e.emp_no = dm.emp_no
+         JOIN departments d on dm.dept_no = d.dept_no
+WHERE dm.to_date = '9999-01-01'
+  AND s.to_date = '9999-01-01'
+ORDER BY dept_name;
+SELECT CONCAT(e.first_name, ' ', e.last_name)      Employee,
+       dept_name                                   Department,
+       CONCAT(e2.first_name, ' ', e2.last_name) AS Manager
+FROM employees e
+         JOIN dept_emp de on e.emp_no = de.emp_no
+         JOIN departments d on de.dept_no = d.dept_no
+         JOIN dept_manager dm on d.dept_no = dm.dept_no
+         JOIN employees e2 on e2.emp_no = dm.emp_no
+WHERE dm.to_date = '9999-01-01'
+  AND de.to_date = '9999-01-01'
+ORDER BY dept_name;
